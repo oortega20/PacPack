d45d7df971e02d50d56e7105b9052322e4c0c735
Oscar Ortega <oscar.g.ortega.5@gmail.com>
2018-07-24 20:48:23 -0700

    minimax with alpha beta

--- myAgentP3.py
+++ myAgentP3.py
@@ -16,10 +16,12 @@
 
 from captureAgents import CaptureAgent
 import random, time, util
+
 from game import Directions
 import game
 from util import nearestPoint
 
+SEARCH_DEPTH = 2
 #########
 # Agent #
 #########
@@ -27,6 +29,7 @@ class myAgentP3(CaptureAgent):
   """
   YOUR DESCRIPTION HERE
   """
+
 
   def registerInitialState(self, gameState):
     """
@@ -48,24 +51,117 @@ class myAgentP3(CaptureAgent):
     # CaptureAgent.registerInitialState in captureAgents.py.
     CaptureAgent.registerInitialState(self, gameState)
     self.start = gameState.getAgentPosition(self.index)
+    self.t_index = list(filter(lambda index: index == self.index, gameState.getPacmanTeamIndices()))[0]
+    self.start_dots = len(gameState.getFood().asList())
 
+
+
+  #returns the optimal plan gor
+  def mini_max(self, state, teammate_plan, plan_so_far, player, depth, alpha, beta):
+    is_win = len(state.getFood().asList()) == 2
+    team_pacman = state.getPacmanTeamIndices()
+    my_pacman = self.index
+    if is_win or depth == 0:
+        return [self.evaluationFunction(state, teammate_plan), plan_so_far]
+    elif player in team_pacman:
+        return self.maximize(state, teammate_plan, plan_so_far, player, depth, alpha, beta)
+    else:
+        return self.minimize(state, teammate_plan, plan_so_far, player, depth, alpha, beta)
+
+
+  def maximize(self, state, teammate_plan, plan_so_far, player, depth, alpha, beta):
+    my_pacman = self.index
+    max_node = [float("-inf"), plan_so_far]
+    actions = actionsWithoutStop(state.getLegalActions(player))
+
+
+    successors = ([state.generateSuccessor(player, a), a] for a in actions)
+    for s,a in successors:
+        next_player = (player + 1) % state.getNumAgents()
+        if player == my_pacman:
+            c_node = self.mini_max(s, teammate_plan, plan_so_far + [a], next_player, depth, alpha, beta)
+        else:
+            c_node = self.mini_max(s, teammate_plan, plan_so_far, next_player, depth, alpha, beta)
+        max_node = max(max_node, c_node, key = lambda x: x[0])
+        value = max_node[0]
+        if value > beta:
+            return max_node
+        alpha = max(alpha, value)
+    return max_node
+
+  def minimize(self, state, teammate_plan, plan_so_far, player, depth, alpha, beta):
+    min_node = [float("inf"), plan_so_far]
+    actions =  actionsWithoutStop(state.getLegalActions(player))
+    successors = [[state.generateSuccessor(player, a), [a] + plan_so_far] for a in actions]
+    for s,a in successors:
+        n_player = (player + 1) % state.getNumAgents()
+        c_node = self.mini_max(s, teammate_plan, plan_so_far, n_player, depth - 1, alpha, beta)
+        min_node = min(min_node, c_node, key = lambda x: x[0])
+        value = min_node[0]
+        if value < alpha:
+            return min_node
+        beta = min(beta, value)
+    return min_node
+
+  #return the value of gamestate in said gameState
+  def evaluationFunction(self, state, teammates_plan):
+    oldFood = state.getFood()
+    pos = state.getAgentPosition(self.index)
+    #filter the dots that your teammate plans on eating
+    f_dots = []
+    for x in range(oldFood.width):
+        for y in range(oldFood.height):
+            if oldFood[x][y] and not (x,y) in teammates_plan:
+                f_dots.append((x,y))
+
+    foodDistances = [self.getMazeDistance(pos, foodPosition) for foodPosition in f_dots]
+    closestFood = float(1) / float(min( foodDistances ) + 1.0)
+
+    ghostPositions = [state.getAgentPosition(ghostIndex) for ghostIndex in self.getOpponents(state)]
+    ghostDistances = [self.getMazeDistance(pos, ghostPosition) for ghostPosition in ghostPositions]
+    ghostDistances.append( 1000 )
+    if min(ghostDistances) < 3:
+        closestGhost = float(1.4) / float(min( ghostDistances ) + 1)
+    else:
+        closestGhost = 0
+    score = self.getScore(state)
+    closestFriend = float(0.2) /  float(self.getMazeDistance(pos, state.getAgentPosition(self.t_index)) + 1.0)
+
+    numRepeats = sum([1 for x in self.observationHistory[-20:] if x.getAgentPosition(self.index) == pos])
+    #try spreading out if stuck?? idk if you have any ideas
+    if numRepeats > 2:
+        numRepeats  *= 10
+    else:
+        numRepeats = 0
+
+    return closestFood + score - closestGhost - numRepeats - closestFriend
   def chooseAction(self, gameState):
     """
     Picks among actions randomly.
     """
     teammateActions = self.receivedBroadcast
+
+    t_future_pos = getFuturePositions(gameState, teammateActions, self.t_index)
+
+    t_plan = getFutureDots(t_future_pos, gameState, self.t_index)
     # Process your teammate's broadcast!
     # Use it to pick a better action for yourself
 
-    actions = gameState.getLegalActions(self.index)
+    plan = self.mini_max(gameState, t_plan, [], self.index, SEARCH_DEPTH, float("-Inf"), float("Inf"))[1]
+    max_action = plan[0]# Change this!
+    futureActions = plan
+    self.toBroadcast = plan
+    return max_action
 
-    filteredActions = actionsWithoutReverse(actionsWithoutStop(actions), gameState, self.index)
-
-    currentAction = random.choice(actions) # Change this!
-    futureActions = None
-
-    self.toBroadcast = futureActions
-    return currentAction
+#returns the dots the agent specified plans on eating
+def getFutureDots(positions, gameState, agent_index):
+    dots = []
+    if positions:
+        uneaten = gameState.getFood().asList()
+        for pos in positions:
+            if pos in uneaten:
+                dots.append(pos)
+    return dots
 
 def actionsWithoutStop(legalActions):
   """
@@ -85,3 +181,25 @@ def actionsWithoutReverse(legalActions, gameState, agentIndex):
   if len (legalActions) > 1 and reverse in legalActions:
     legalActions.remove(reverse)
   return legalActions
+
+def getFuturePositions(gameState, plannedActions, agentIndex):
+    """
+    Returns list of future positions given by a list of actions for a
+    specific agent starting form gameState
+
+    NOTE: this does not take into account other agent's movements
+    (such as ghosts) that might impact the *actual* positions visited
+    by such agent
+    """
+    if plannedActions is None:
+      return None
+
+    planPositions = [gameState.getAgentPosition(agentIndex)]
+    for action in plannedActions:
+      if action in gameState.getLegalActions(agentIndex):
+        gameState = gameState.generateSuccessor(agentIndex, action)
+        planPositions.append(gameState.getAgentPosition(agentIndex))
+      else:
+        print("Action list contained illegal actions")
+        break
+    return planPositions
